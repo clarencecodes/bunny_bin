e47d3b35c98e1e11881cc02b6eb074fab90b7809
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-23 22:15:39 +0800

    try printing in inches

--- client.py
+++ client.py
@@ -5,5 +5,7 @@ divider = MCP3008(0)
 
 while True:
     print(divider.value)
-    print(str(28250/(divider.value-229.5))+"centimeters")
+    # print(str(28250/(divider.value-229.5))+"centimeters")
+    inches = 6202.3*divider.value^-1.056
+    print(str(inches))
     time.sleep(1)
