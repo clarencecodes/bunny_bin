baa90ed9cf288b4c1139856f0e8adea6df868126
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-23 22:38:10 +0800

    Update client.py

--- client.py
+++ client.py
@@ -7,8 +7,10 @@ while True:
     print(divider.value)
     # print(str(28250/(divider.value-229.5))+"centimeters")
 
-    v = (divider.value / 1023.0) * 3.3
-    dist = 16.2537 * v ** 4 - 129.893 * v ** 3 + 382.268 * v ** 2 - 512.611 * v + 301.439
-    print("Distance {:.2f}".format(dist))
+    # v = (divider.value / 1023.0) * 3.3
+    # dist = 16.2537 * v ** 4 - 129.893 * v ** 3 + 382.268 * v ** 2 - 512.611 * v + 301.439
+
+    # print(str(round(divider.value / 0.05) * 0.05))
+    # print("Distance {:.2f}".format(dist))
 
     time.sleep(1)
