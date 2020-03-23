cebbe5675d8f26285980420977c7d34050ff8f06
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-23 22:20:10 +0800

    Update client.py

--- client.py
+++ client.py
@@ -6,6 +6,9 @@ divider = MCP3008(0)
 while True:
     print(divider.value)
     # print(str(28250/(divider.value-229.5))+"centimeters")
-    inches = 6202.3*divider.value^-1.056
-    print(str(inches))
+
+    v = (mcp.read_adc(0) / 1023.0) * 3.3
+    dist = 16.2537 * v ** 4 – 129.893 * v ** 3 + 382.268 * v ** 2 – 512.611 * v + 301.439
+    print("Distance {:.2f}".format(dist))
+
     time.sleep(1)
