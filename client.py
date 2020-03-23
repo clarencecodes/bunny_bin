9789cef0fcfc052e1be9f927fdcadf04db11a126
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-23 22:21:28 +0800

    fix bug

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ while True:
     print(divider.value)
     # print(str(28250/(divider.value-229.5))+"centimeters")
 
-    v = (mcp.read_adc(0) / 1023.0) * 3.3
+    v = (divider.value / 1023.0) * 3.3
     dist = 16.2537 * v ** 4 - 129.893 * v ** 3 + 382.268 * v ** 2 - 512.611 * v + 301.439
     print("Distance {:.2f}".format(dist))
 
