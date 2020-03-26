b91ed2b0ac0b2a4485e41edd54466698fcc613df
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-26 23:16:21 +0800

    API integration with Twilio

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket, pickle, threading
+from twilio.rest import Client
 
 HOST = '0.0.0.0'
 PORT = 12346
@@ -20,11 +21,25 @@ def clientthread(conn, addr):
                 print(deserialized_message)
 
                 if deserialized_message in TRASHBINS:
-                    print(TRASHBINS[d])
-                    print("Send HTTP request to twilio")
-                    conn.send(message)
+                    print("Sending HTTP request to Twilio...")
+                    triggerSMSToCleaner(TRASHBINS[deserialized_message], '+65')
+
         except:
             continue
+
+def triggerSMSToCleaner(dustbin_location, mobile_number):
+    account_sid = 'redacted'
+    auth_token = 'redacted'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body=f"Trash bin at {dustbin_location} is full! Please empty me!",
+        from_='Bunny Bin',
+        to=mobile_number
+    )
+
+    print(message.sid)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
