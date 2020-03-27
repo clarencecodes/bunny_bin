ff7e46218e7d71e935a4da305e1bff4676b21bbc
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-03-28 00:26:11 +0800

    Allow client user to key in IP address and port number as argument

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
 import time, socket, pickle
 from gpiozero import MCP3008
-
-HOST = '172.20.10.13' # IP Address of device running server.py
-PORT = 12345
+import sys
 
 TRASHBIN = 'blue'
+
+# checks whether sufficient arguments have been provided
+if len(sys.argv) != 3:
+    print("Invalid Command")
+    print("Format: server <HOST IP> <PORT NUMBER>")
+    exit()
+
+# IP Address and Port will be based on device running server.py
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
 
 divider = MCP3008(0)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST, PORT))
+server.connect((IP_address, Port))
 while True:
     print(divider.value)
     
